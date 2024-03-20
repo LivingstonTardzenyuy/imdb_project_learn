f093bd7dd771013d90a38930bd0064b6dfe848ad
LivingstonTardzenyuy <kongnyuylivingston2@gmail.com>
2024-03-20 16:39:07 +0100

    getting use with serializers

--- watchmate/watchlist_app/views.py
+++ watchmate/watchlist_app/views.py
@@ -1,23 +1,23 @@
-from django.shortcuts import render
-from watchlist_app.models import Movie 
-from django.http import JsonResponse
+# from django.shortcuts import render
+# from watchlist_app.models import Movie 
+# from django.http import JsonResponse
 
-# Create your views here.
-def movie_list(request):
-    movies = Movie.objects.all()
-    data = {
-        'movies': list(movies.values())
-    }
-    # print(movies.values())
-    return JsonResponse(data)
+# # Create your views here.
+# def movie_list(request):
+#     movies = Movie.objects.all()
+#     data = {
+#         'movies': list(movies.values())
+#     }
+#     # print(movies.values())
+#     return JsonResponse(data)
     
     
-def movie_detail(request, pk):
-    movie = Movie.objects.get(pk = pk)
-    data = {
-        'movie': movie.name,
-        'description': movie.description,
-        'active': movie.active 
-    }
-    return JsonResponse(data) 
+# def movie_detail(request, pk):
+#     movie = Movie.objects.get(pk = pk)
+#     data = {
+#         'movie': movie.name,
+#         'description': movie.description,
+#         'active': movie.active 
+#     }
+#     return JsonResponse(data) 
 
