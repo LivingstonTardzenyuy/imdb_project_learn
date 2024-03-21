676e1b7ccb8a75280c0a7b485ab8456c27c3af15
LivingstonTardzenyuy <kongnyuylivingston2@gmail.com>
2024-03-21 10:31:24 +0100

    adding a post method

--- watchmate/watchlist_app/api/views.py
+++ watchmate/watchlist_app/api/views.py
@@ -3,14 +3,23 @@ from watchlist_app.api.serializers import MovieSerializers
 from rest_framework.response import Response 
 from rest_framework.decorators import api_view
 
-@api_view(["GET",])
+@api_view(["GET", "POST"])
 def movie_list(request):
-    movies = Movie.objects.all()
-    serializer = MovieSerializers(movies, many = True)
-    return Response(serializer.data)
+    if request.method == "GET":
+        movies = Movie.objects.all()
+        serializer = MovieSerializers(movies, many = True)
+        return Response(serializer.data)
 
+    if request.method == "POST":
+        serializer = MovieSerializers(data = request.data) 
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)       
 @api_view(["GET",])
 def movie_details(request, pk):
     movie = Movie.objects.get(pk=pk)
     serializer = MovieSerializers(movie)
     return Response(serializer.data)
+
