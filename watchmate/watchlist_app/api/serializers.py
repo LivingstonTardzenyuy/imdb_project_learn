9ae7d8b914e03bffc06460385a327d266dfd26c2
LivingstonTardzenyuy <kongnyuylivingston2@gmail.com>
2024-03-29 05:39:02 +0100

    created a review models

--- watchmate/watchlist_app/api/serializers.py
+++ watchmate/watchlist_app/api/serializers.py
@@ -1,28 +1,14 @@
 from rest_framework import serializers
-from watchlist_app.models import WatchList, StreamPlatForm
+from watchlist_app.models import WatchList, StreamPlatForm, Reviews
 
 
-
-       
-class StreamPlatFormSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = StreamPlatForm
-        fields = "__all__"
-        
-        def validate(self, data):
-            if data['name'] == data['about']:
-                raise serializers.ValidationError("Both fields should be different")
-            else:
-                return data 
-
+# class ReviewsSerializer(serializers.ModelSerializer):
+    
 class WatchListSerializer(serializers.ModelSerializer):
     len_names = serializers.SerializerMethodField()  #adding a new field without specifying in models.
     class Meta:
         model = WatchList 
-        fields = '__all__'
-
-
-       
+        fields = '__all__'       
     def get_len_names(self, object):
         return len(object.name)
 
@@ -33,4 +19,18 @@ class WatchListSerializer(serializers.ModelSerializer):
         else:
             return data 
         
- 
+class StreamPlatFormSerializer(serializers.ModelSerializer):
+        # watchlist = serializers.HyperlinkedRelatedField(
+    #     read_only = True, many = True, view_name = 'movie-detail')  #doing the reverse relationship
+    watchlist = WatchListSerializer(many = True, read_only = True)  
+    class Meta:
+    
+        model = StreamPlatForm
+        fields = "__all__"
+        
+        def validate(self, data):
+            if data['name'] == data['about']:
+                raise serializers.ValidationError("Both fields should be different")
+            else:
+                return data 
+
--- watchmate/watchlist_app/models.py
+++ watchmate/watchlist_app/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.core.validators import MinLengthValidator, MaxLengthValidator
 # Create your models here.
   
 class StreamPlatForm(models.Model):
@@ -21,4 +21,13 @@ class WatchList(models.Model):
     def __str__(self):
         return self.name
     
-  
+class Reviews(models.Model):
+    rating = models.PositiveBigIntegerField(validators = [MinLengthValidator(1), MaxLengthValidator(5)])
+    description = models.CharField(max_length = 200, null = True)
+    created = models.DateTimeField(auto_now_add = True)
+    update = models.DateTimeField(auto_now = True)
+    active = models.BooleanField(default = True)
+    watchlist = models.ForeignKey(WatchList, on_delete = models.CASCADE, related_name = "reviews")
+    
+    def __str__(self):
+        self.rating 
