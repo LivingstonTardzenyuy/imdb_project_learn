535e6cd58242b47a1a0373d36c27cf04259c1896
LivingstonTardzenyuy <kongnyuylivingston2@gmail.com>
2024-04-18 02:58:59 +0100

    g

---
+++ watchmate/users_app/api/views.py
@@ -0,0 +1,24 @@
+from users_app.api.serializers import UserRegistrationSerializer
+from rest_framework.decorators import api_view 
+from rest_framework import status 
+from rest_framework.response import Response 
+from rest_framework.authtoken.models import Token
+from users_app.models import *
+
+@api_view(['POST'],)
+def registration_view(request):
+    if request.method == 'POST':
+        serializer = UserRegistrationSerializer(data=request.data)
+        
+        data = {}
+        if serializer.is_valid():
+            data['response'] ='Registered successfully'
+            account = serializer.save()
+            data['username'] = account.username
+            data['emal'] = account.email
+            token = Token.objects.get(user = account).key
+            data['token'] = token 
+            # return Response(data, status=status.HTTP_201_CREATED) q 
+        else:
+            data['response'] = serializer.errors
+        return Response(data)
