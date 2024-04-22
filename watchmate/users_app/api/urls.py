6c9b582a000f02a539c7f8b1c73fe1043919cd4a
LivingstonTardzenyuy <kongnyuylivingston2@gmail.com>
2024-04-22 02:24:56 +0100

    adding endpoints for JWT

--- watchmate/users_app/api/urls.py
+++ watchmate/users_app/api/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path 
 from rest_framework.authtoken import views
 from users_app.api.views import registration_view, logout_view
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,TokenRefreshView,)
+
 
 urlpatterns = [
     path('login/', views.obtain_auth_token),
     path('registration/', registration_view, name='registration'),
     path('logout/', logout_view, name='logout'),
+    
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
+
