bfaa62e49f9e80d8a4f2dfba4f8488fddb3e8ee2
LivingstonTardzenyuy <kongnyuylivingston2@gmail.com>
2024-04-26 02:05:29 +0100

    added throttle class to control the number of request

--- watchmate/watchmate/settings.py
+++ watchmate/watchmate/settings.py
@@ -140,7 +140,15 @@ REST_FRAMEWORK = {
         # 'rest_framework_simplejwt.authentication.JWTAuthentication',
         'rest_framework.authentication.TokenAuthentication',
         # 'rest_framework.authentication.SessionAuthentication',
-    ]
+    ],
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle'
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '1/second',
+        'user': '4/second'
+    }
 }
 
 
