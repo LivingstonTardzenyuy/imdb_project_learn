e730c89cf21505c880fb160ed58ab6f23e4ef100
LivingstonTardzenyuy <kongnyuylivingston2@gmail.com>
2024-05-02 02:29:33 +0100

    writig the endpoint for testing new user

--- watchmate/users_app/tests.py
+++ watchmate/users_app/tests.py
@@ -1,2 +1,20 @@
 from rest_framework.test import APITestCase
+from django.urls import reverse 
+from rest_framework.test import status
+from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token 
+
+
+
 # Create your tests here.
+class RegisterTestCase(APITestCase):
+    def test_register(self):
+        data = {
+            "username": "testCase",
+            "password": "NewPassword",
+            "password2": "NewPassword2",
+            
+        }
+        
+        response = self.client.post(reverse("registration"), data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
